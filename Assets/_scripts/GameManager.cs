bb2db2ecc33890cc161ead73123a7344023694f2
Eder Diaz <ederchrono@gmail.com>
2017-04-12 16:31:02 -0500

    Setup Serializable fields

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+	// Fields to be filled from the Unity UI
+	[SerializeField]
+	private GameObject leftPaddle;
+	[SerializeField]
+	private GameObject rightPaddle;
+	[SerializeField]
+	private GameObject ball;
+	[SerializeField]
+	private Text leftScoreText;
+	[SerializeField]
+	private Text rightScoreText;
 
 	// Use this for initialization
 	void Start () {
