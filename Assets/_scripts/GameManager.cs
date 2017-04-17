3d9461967aa683c383c2c90f1b8bab52a042df16
Eder Diaz <ederchrono@gmail.com>
2017-04-17 09:15:27 -0500

    Add player movement

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -17,6 +17,8 @@ public class GameManager : MonoBehaviour {
 	[SerializeField]
 	private Text rightScoreText;
 
+	private float paddleVelY=5;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -24,6 +26,29 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		UpdatePlayerPaddle ();
+		UpdateAIPaddle ();
+		UpdateBall ();
+	}
+
+	void UpdatePlayerPaddle(){
+		int paddleDirection = 0;
+		if (Input.GetKey ("down")) {
+			//print ("down is pressed!");
+			paddleDirection--;
+		}
+		if (Input.GetKey ("up")) {
+			paddleDirection++;
+		}
+		Vector3 paddleMovement = new Vector3 (0, paddleDirection * paddleVelY * Time.deltaTime, 0);
+		rightPaddle.transform.position += paddleMovement;
+
+	}
+
+	void UpdateAIPaddle(){
+
+	}
+	void UpdateBall(){
+
 	}
 }
